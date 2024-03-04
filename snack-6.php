bc756dcec4110b28bec4b0e84a9534dbb50551d1
filippogiovannoni <filippogiovannonii@gmail.com>
2024-03-04 14:58:26 +0100

    add Snack 6

---
+++ snack-6.php
@@ -0,0 +1,59 @@
+<?php
+
+// Snack 6
+// Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php foreach ($db as $category => $list) : ?>
+
+        <div class="<?php echo $category === 'teachers' ? 'bg-success' : 'bg-secondary' ?>">
+
+            <?php foreach ($list as $person) : ?>
+
+                <span class="d-block p-1"><?php echo $person['name'] . ' ' . $person['lastname'] ?></span>
+
+            <?php endforeach ?>
+
+        </div>
+
+    <?php endforeach; ?>
+
+</body>
+
+</html>
